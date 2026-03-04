1b9ad2ad777cf9af239b1b9ca52b698a18ae3aac
tmn281196 <tmn.vietnam@gmail.com>
2026-03-04 11:08:56 +0700

    update program, model, firmware

---
+++ CCS_Workspace/8TX4RX_R_NEW/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -llibmath.a -l"C:/Users/Com26/workspace_ccstheia/8TX4RX_R_NEW/captivate/BASE/libraries/captivate_fr2633_family.lib" -l"C:/Users/Com26/workspace_ccstheia/8TX4RX_R_NEW/driverlib/MSP430FR2xx_4xx/libraries/driverlib_fr2633_family.lib" -l"C:/Users/Com26/workspace_ccstheia/8TX4RX_R_NEW/mathlib/libraries/QmathLib.lib" -l"C:/Users/Com26/workspace_ccstheia/8TX4RX_R_NEW/mathlib/libraries/IQmathLib.lib" -llibc.a
+
